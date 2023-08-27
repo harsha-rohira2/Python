d540cf1a5156a78a33e73221d605e5cf2a2c56db
Harsha Rohira <84343894+harsha-rohira2@users.noreply.github.com>
2023-08-27 11:04:58 +0530

    Add files via upload

--- Basic python.py
+++ Basic python.py
@@ -22,6 +22,39 @@ y="Apple"
 print(type(x))
 print(type(y))
 
-"""
 # print multiple variable in one line
 x,y,z="red","Blue","Green"
+print(x,y,z)
+
+# print one value in multiple Variable
+x=y=z="Blue"
+print(x)
+print(y)
+print(z)
+
+#print number in python
+a=2
+b=2.8
+c=2j
+print(a)
+print(b)
+print(c)
+
+#Type Conversion in python
+a=2
+b=2.9
+c=6j
+x=float(a)
+y=complex(b)
+z=int(a)
+print(x)
+print(y)
+print(z)
+
+# Print Random Number in python
+import random
+print(random.randrange(1,10))
+
+"""
+
+
