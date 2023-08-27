0fc31ace28aa955641141a042f5310e92208bc5c
Harsha Rohira <84343894+harsha-rohira2@users.noreply.github.com>
2023-08-27 10:50:03 +0530

    Add files via upload

---
+++ Basic python.py
@@ -0,0 +1,27 @@
+"""
+# Print hello world in Python
+print("Hello World")
+
+# Variable in Python
+a=10
+b='Harsha'
+c="Rohira"
+print(a)
+print("First Name : ",b)
+print("LAst Name :",c)
+
+# Casting in Python
+x=str(20)
+y=int(20)
+z=float(20)
+print(x,y,z)
+
+# Print Data type
+x=5
+y="Apple"
+print(type(x))
+print(type(y))
+
+"""
+# print multiple variable in one line
+x,y,z="red","Blue","Green"
