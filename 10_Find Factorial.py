240158ed210746fa3da5101669d9b0abdf4cecdc
Harsha Rohira <84343894+harsha-rohira2@users.noreply.github.com>
2023-09-11 12:57:45 +0530

    Update 10_Find Factorial.py

--- 10_Find Factorial.py
+++ 10_Find Factorial.py
@@ -7,3 +7,15 @@ for i in range(1, n + 1):
 
 print("The factorial is :")
 print(fact)
+
+# Using Functions
+def factorial(num):
+    fact=1
+    i=1
+    if num<0:
+        print("Enter number: ")
+    else:
+        while(i<=num):
+            fact*=i
+            i=i+1
+    return fact
